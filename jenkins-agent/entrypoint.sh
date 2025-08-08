d2098069d651a944fc764b758a8a97b99c7fd1c7
Krutika09 <krutikayawale2002@gmail.com>
2025-08-08 09:37:56 +0530

    .\jenkins-agent\

--- jenkins-agent/entrypoint.sh
+++ jenkins-agent/entrypoint.sh
@@ -7,8 +7,8 @@ fi
 
 # Start Jenkins agent
 exec java -jar agent.jar \
-  -url http://34.216.17.78:8080/ \
-  -secret c3e3f47b96a740c5f2e04adfd4a127f9a31608da3e06df9b5be2e5c20d979207 \
-  -name "k8-agent" \
+  -url http://44.251.64.138:8080/ \
+  -secret dd07d637328e1f74f8adc48ee3b11ca55524e34ba7f48b705a8d4ab2560a916a \
+  -name "K8-Agent-Secrets" \
   -webSocket \
   -workDir "/home/jenkins/agent"
